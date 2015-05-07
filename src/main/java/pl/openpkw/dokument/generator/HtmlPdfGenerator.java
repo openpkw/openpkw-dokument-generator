95ecc056b64a6691d28bfaa1edfb778ba86c5c4c
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-05-07 23:19:35 +0200

    Pierwsza implementacja dodawania QR kodów

--- src/main/java/pl/openpkw/dokument/generator/HtmlPdfGenerator.java
+++ src/main/java/pl/openpkw/dokument/generator/HtmlPdfGenerator.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -12,25 +13,29 @@ import org.apache.velocity.VelocityContext;
 
 import pl.openpkw.dokument.generator.webservice.dto.Form;
 
-
 @Singleton
 public class HtmlPdfGenerator {
-    
+
     @Inject
     private VelocityEngine velocity;
 
     @Inject
     private PdfRenderer pdfRenderer;
 
+    @Inject
+    private QRCodeGenerator qrCodeGenerator;
+
     /**
      * Tworzy dokument PDF na podstawie szablonu HTML oraz danych z formularza
-     * @param formData
-     * @return 
      */
     public byte[] generate(Form formData) {
         try {
             Path workingDirectory = Files.createTempDirectory("openpkw-");
             Path htmlFile = generateHtmlFileFromTemplate("/templates/PdfTemplate.html", formData, workingDirectory);
+
+            byte[] qrCode = generateQRCodeFromFormData(formData);
+            saveFileInWorkingDirectory("qrcode.jpeg", workingDirectory, qrCode);
+
             copyFileToWorkingDirectory("/templates/styles.css", workingDirectory);
             return pdfRenderer.render(htmlFile);
         } catch (Exception ex) {
@@ -53,6 +58,11 @@ public class HtmlPdfGenerator {
         }
     }
 
+    private byte[] generateQRCodeFromFormData(Form formData) {
+        String jsonData = qrCodeGenerator.createJsonDataForQRCode(formData);
+        return qrCodeGenerator.generate(jsonData);
+    }
+
     private void copyFileToWorkingDirectory(String filePath, Path workingDirectory) {
         try {
             InputStream sourceFile = this.getClass().getResourceAsStream(filePath);
@@ -61,6 +71,15 @@ public class HtmlPdfGenerator {
             Files.copy(sourceFile, targetFilePath);
         } catch (Exception ex) {
             throw new RuntimeException("Failed to copy file " + filePath + " to working directory: " + ex.getMessage(), ex);
+        }
+    }
+
+    private void saveFileInWorkingDirectory(String targetFileName, Path workingDirectory, byte[] fileContent) {
+        try {
+            Path targetFilePath = Paths.get(workingDirectory.toString(), targetFileName);
+            Files.write(targetFilePath, fileContent, StandardOpenOption.CREATE);
+        } catch (Exception ex) {
+            throw new RuntimeException("Failed to create file " + targetFileName + " in working directory: " + ex.getMessage(), ex);
         }
     }
 }
---
+++ src/test/java/pl/openpkw/dokument/generator/webservice/ManualTest.java
@@ -0,0 +1,47 @@
+package pl.openpkw.dokument.generator.webservice;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+public class ManualTest {
+
+    public static void main(String[] args) {
+        new ManualTest().run();
+    }
+
+    public void run() {
+        try {
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client.target("http://localhost:8080/openpkw-dokument-generator/service/protocol");
+
+            String form = new String(Files.readAllBytes(Paths.get(this.getClass().getResource("/form.json").toURI())));
+            System.out.println(form);
+
+            byte[] response = target.request().post(Entity.entity(form, MediaType.APPLICATION_JSON), byte[].class);
+            
+            System.out.println("Received "+response.length+" bytes.");
+            
+            File outputFile = new File("/temp/output.pdf");
+            if (outputFile.exists()) {
+                outputFile.delete();
+            }
+
+            
+            FileOutputStream outputFileStream = new FileOutputStream(outputFile);
+            outputFileStream.write(response);
+            outputFileStream.close();
+            
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+}
