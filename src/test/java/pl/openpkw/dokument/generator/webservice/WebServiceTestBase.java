57abb2a9722722624054a7dd084a369d1990ee7f
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-11-11 15:03:18 +0100

    Wprowadzenie plugina Failsafe do testów akceptacyjnych, skonfigurowanie Failsafe i Surefire

---
+++ src/test/java/pl/openpkw/dokument/generator/webservice/WebServiceTestBase.java
@@ -0,0 +1,8 @@
+package pl.openpkw.dokument.generator.webservice;
+
+public class WebServiceTestBase {
+    
+    protected final static String LOCALHOST = "localhost:8080";
+    protected final static String TEST = "dobromir.openpkw.pl:9080";
+    protected final static String UAT = "pat.openpkw.pl:9080";
+}
